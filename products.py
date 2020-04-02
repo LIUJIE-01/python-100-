df16d99eea9f8e660cd35362355cd4a820db4f6c
LIUJIE-01 <58101181+LIUJIE-01@users.noreply.github.com>
2020-04-02 17:19:30 +0800

    Add files via upload

---
+++ products.py
@@ -0,0 +1,19 @@
+products = []
+
+while True:
+    name = input('请输入商品名称: ')
+    if name == 'q':
+        break
+    price = input('请输入商品价格: ')
+    price = int(price)
+    products.append([name, price])
+print(products)
+for p in products:
+    print(p[0], '的价格', p[1])
+
+
+with open('123.csv', 'w', encoding='utf-8') as f:
+    f.write('商品, 价格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
+
